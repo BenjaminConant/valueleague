9c01552fbb802cf7d94c2a32b7c56262746209b1
Steve Kwak <steveguac@gmail.com>
2015-04-19 09:42:02 -0400

    before merge

--- client/app/standings/standings.controller.js
+++ client/app/standings/standings.controller.js
@@ -6,36 +6,58 @@ angular.module('valueleagueApp')
     {
     	standing: "Standing",
     	name:"Name",
-    	score: "Score"
+    	stockSym:"Symbol",
+    	stockName:"Stcok Name",
+    	Change:"Change",
+    	score: "Score",
+    	titleRow: true
     },
     {
     	standing: "1",
     	name:"Steve",
+    	stockSym:"YUM",
+    	stockName:"Yum Brands Inc",
+    	Change:"0.67%",
     	score: "500"
     },
     {
     	standing: "2",
     	name:"Katrina",
-    	score: "388"
+    	stockSym:"ZTS",
+    	stockName:"Zoetis Inc",
+    	Change:"-0.17%",
+    	score: "400"
     },
     {
     	standing: "3",
     	name:"Ben",
+    	stockSym:"ZMH",
+    	stockName:"Zimmer Holdings Inc",
+    	Change:"-0.72%",
     	score: "350"
     },
     {
     	standing: "4",
     	name:"Patrick",
-    	score: "310"
+    	stockSym:"ZION",
+    	stockName:"Zions Bancorp",
+    	Change:"-0.82%",
+    	score: "300"
     },
     {
-    	standing: "6",
+    	standing: "500",
     	name:"Evan",
-    	score: "290"
+    	stockSym:"XYL",
+    	stockName:"Xylem Inc",
+    	Change:"-1.44%",
+    	score: "250"
     },
     {
     	standing: "6",
     	name:"Anirban",
+    	stockSym:"YHOO",
+    	stockName:"Yahoo Inc",
+    	Change:"-2.12%",
     	score: "150"
     }
     ];
