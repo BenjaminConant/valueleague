36b2adef0de4f3c8c4ef60e3ed57afefebaefc9a
Steve Kwak <steveguac@gmail.com>
2015-04-18 23:17:57 -0400

    committed

--- client/app/standings/standings.controller.js
+++ client/app/standings/standings.controller.js
@@ -2,5 +2,42 @@
 
 angular.module('valueleagueApp')
   .controller('StandingsCtrl', function ($scope) {
-    $scope.message = 'Hello';
+    $scope.leaders = [
+    {
+    	standing: "Standing",
+    	name:"Name",
+    	score: "Score"
+    },
+    {
+    	standing: "1",
+    	name:"Steve",
+    	score: "500"
+    },
+    {
+    	standing: "2",
+    	name:"Katrina",
+    	score: "388"
+    },
+    {
+    	standing: "3",
+    	name:"Ben",
+    	score: "350"
+    },
+    {
+    	standing: "4",
+    	name:"Patrick",
+    	score: "310"
+    },
+    {
+    	standing: "6",
+    	name:"Evan",
+    	score: "290"
+    },
+    {
+    	standing: "6",
+    	name:"Anirban",
+    	score: "150"
+    }
+    ];
+
   });
